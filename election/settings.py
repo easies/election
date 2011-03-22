5f1fec5ea9585b189cca334f7206a609612ba230
Alex Lee <lee@ccs.neu.edu>
2011-03-21 20:39:20 -0400

    Move the old project into a separate directory.

--- election/settings.py
+++ election/settings.py
@@ -1,33 +1,33 @@
-# Django settings for acm_election project.
+# Django settings for election project.
 
-from os.path import normpath, join, dirname, abspath
-
-ROOT = lambda *base : normpath(join(dirname(__file__), *base)).replace('\\','/')
-MODULE = normpath(abspath(dirname(__file__))).replace('\\','/').split('/')[-1]
-
-DEBUG = False 
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('John', 'jmendel@ccs.neu.edu'),
-    ('Alex', 'lee@ccs.neu.edu'),
+    # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'       # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'acm_election2009' # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'                # Not used with sqlite3.
-DATABASE_PASSWORD = 'singlesignon'            # Not used with sqlite3.
-DATABASE_HOST = ''                # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''                # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -37,44 +37,50 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = False
+USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: '/home/media/media.lawrence.com/'
-MEDIA_ROOT = ROOT('media') + '/'
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
-# Examples: 'http://media.lawrence.com', 'http://example.com/media/'
-MEDIA_URL = 'http://acm.ccs.neu.edu/election/media/'
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: 'http://foo.com/media/', '/media/'.
-ADMIN_MEDIA_PREFIX = '/library/adminmedia/'
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '#wi=h%hd$r1pfefn+co@la4f9j(4mzy+yha!74ulmxwp9cstw-'
+SECRET_KEY = 'ye*!^p-u)dd5+39#crk*j*!1@53$6a(+-1yt2qav#6=kcz+%n^'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_cas.middleware.CASMiddleware',
-    'django.middleware.doc.XViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = MODULE + '.stagedurls'
+ROOT_URLCONF = 'election.urls'
 
 TEMPLATE_DIRS = (
-   ROOT('templates')
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
@@ -82,26 +88,9 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.admin',
-    MODULE + '.candidates',
-    MODULE + '.voting',
+    'django.contrib.messages',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
-
-AUTHENTICATION_BACKENDS = (
-    ##'django.contrib.auth.backends.ModelBackend',
-    'django_cas.backends.CASBackend',
-)
-
-LOGIN_URL = '/election/login/'
-LOGOUT_URL = '/election/logout/'
-LOGIN_REDIRECT_URL = '/election/'
-CAS_SERVER_URL = 'https://vzcgi.ccs.neu.edu/sso/cas/'
-
-AUDIT_LOG = ROOT('audit.log')
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
-    'django.core.context_processors.debug',
-    MODULE + '.util.context_processors.acm_election'
-)
-
--- election/urls.py
+++ election/urls.py
@@ -1,39 +1,16 @@
 from django.conf.urls.defaults import *
-from settings import MEDIA_ROOT, MODULE
-from django.contrib import admin
 
-admin.autodiscover()
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/(.*)', admin.site.root),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                              { 'document_root' : MEDIA_ROOT }),
-    (r'^$', MODULE + '.views.index'),
-)
+    # Example:
+    # (r'^election/', include('election.foo.urls')),
 
-urlpatterns += patterns(MODULE + '.candidates.views',
-    (r'^candidates/$', 'index'),
-    (r'^candidates/(?P<username>[a-zA-Z0-9]+)/$', 'get_candidate'),
-    (r'^candidates/(?P<username>[a-zA-Z0-9]+)/add/$', 'add_candidate'),
-    (r'^candidates/(?P<username>[a-zA-Z0-9]+)/edit/$', 'edit_candidate'),
-    (r'^candidates/(?P<username>[a-zA-Z0-9]+)/remove/$', 'remove_candidate'),
-    
-    (r'^offices/$', 'office_index'),
-    (r'^offices/add/$', 'add_office'),
-    (r'^offices/(?P<id>[0-9]+)/$', 'get_office'),
-    (r'^offices/(?P<id>[0-9]+)/edit/$', 'edit_office'),
-    (r'^offices/(?P<id>[0-9]+)/remove/$', 'remove_office'),
-)
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-urlpatterns += patterns(MODULE + '.voting.views',
-    (r'^voting/', 'index'),
-    (r'^results/', 'results'),
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
 )
-    
-# cas
-urlpatterns += patterns('django_cas.views',
-    url(r'^login/$', 'login', name='login'),
-    url(r'^logout/$', 'logout', name='logout'),
-    url(r'^logout/\?next=(?P<next_page>.+)$', 'logout', name='logout-with-next'),
-)
-
