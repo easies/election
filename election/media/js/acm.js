492fe9dea86db18f6b73e67c9d20a81e1a42062e
Alex Lee <lee@ccs.neu.edu>
2011-03-22 02:41:09 -0400

    Fixup HTML, javascript, CSS

--- election/media/js/acm.js
+++ election/media/js/acm.js
@@ -1,23 +1,11 @@
-function on_load() {
-    for (var i = 0, len = on_load.load.length; i < len; i++) {
-        on_load.load[i]();
-    }
-    return true;
-}
-
-on_load.load = []
-on_load.add = function(f) {
-    on_load.load.push(f);
-};
-
-function OfficeDesc(obj, key) {
+function office_dialog(obj, url) {
     // Reset all the buttons
-    $(".office").css("color","#336699");
+    $(".office").css("color", "#336699");
     // Highlight the current office
     $(obj).css("color", "red");
     // Fetch the data.
     $.ajax({
-        url: "/json/office/" + key + "/",
+        url: url,
         success: function(office) {
             $("#office_info").find("#title").text(office.title);
             $("#office_info").find("#desc_in").html(office.description);
