282ff41f6f1568ab96e4d6403a4d363c93e4d360
Alex Lee <lee@ccs.neu.edu>
2011-03-28 13:26:59 -0400

    Use jQueryUI's dialog.

--- election/media/js/acm.js
+++ election/media/js/acm.js
@@ -1,21 +1,11 @@
-function office_dialog(obj, url) {
-    // Reset all the buttons
-    $(".office").css("color", "#336699");
-    // Highlight the current office
-    $(obj).css("color", "red");
-    // Fetch the data.
-    $.ajax({
-        url: url,
-        success: function(office) {
-            $("#office_info").find("#title").text(office.title);
-            $("#office_info").find("#desc_in").html(office.description);
-            $("#office_info").show();
-        }
-    });
-    $("#office_info").find("#close").bind("click", function() {
-        $("#office_info").hide();
-        // Undo obj stuff
-        $(".office").css("color","#336699");
-        return false;
-    });
+function office_dialog(office_url) {
+  $.ajax({
+    url: office_url,
+    success: function (office) {
+      var x = $("<div>" + office.description + "</div>");
+      x.dialog({
+        title: office.title,
+      });
+    }
+  });
 }
